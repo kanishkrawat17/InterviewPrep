70a8888521f5bb0cbcf2cbae649a625ab5cf127d
kanishkrawat17 <kanishkrawat67@gmail.com>
2024-12-07 03:05:37 -0800

    added currying

--- JS/currying.js
+++ JS/currying.js
@@ -1,10 +1,23 @@
 function curringFunc(a) {
-    return function(b) {
-        if(b) {
-            return curringFunc(a+b);
+    return function (b) {
+        if (b) {
+            return curringFunc(a + b);
         }
         return a;
     }
 };
 
-console.log(curringFunc(1)(2)(3)())
+console.log(curringFunc(1)(2)(5)(), "Ans-1")
+
+
+/** Question: 2 */
+
+function sumCurrying(...args) {
+    const sum = args.reduce((acc, num) => acc + num, 0);
+    return function (...innerArgs) {
+        if (innerArgs.length==0) return sum;
+        return sumCurrying(sum, ...innerArgs); 
+    }
+}
+
+console.log(sumCurrying(2,1,0)(1,3)(3), 'Ans-2')
